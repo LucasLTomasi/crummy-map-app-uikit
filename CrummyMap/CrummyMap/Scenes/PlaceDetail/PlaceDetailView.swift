d42b93e0be709154c6b2e12023dd3343cefd159b
Lucas Tomasi <lucasltomasi@gmail.com>
2021-07-08 20:19:33 -0300

    Add map view to place detail screen.

--- CrummyMap/CrummyMap/Scenes/PlaceDetail/PlaceDetailView.swift
+++ CrummyMap/CrummyMap/Scenes/PlaceDetail/PlaceDetailView.swift
@@ -1,13 +1,22 @@
+import MapKit
 import UIKit
 
 class PlacesDetailView: UIView {
+    private let contentSpacing: CGFloat = 8
+
     var formattedPlaceLabel = FormattedPlaceLabel()
+    let mapView: MKMapView = {
+        var mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
 
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
         buildViewHierarchy()
         setupConstraints()
         formattedPlaceLabel.textAlignment = .center
+        formattedPlaceLabel.font = formattedPlaceLabel.font.withSize(24)
         backgroundColor = .systemBackground
     }
 
@@ -17,14 +26,18 @@ class PlacesDetailView: UIView {
 
     private func buildViewHierarchy() {
         addSubview(formattedPlaceLabel)
+        addSubview(mapView)
     }
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            formattedPlaceLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            formattedPlaceLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-            formattedPlaceLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
-            formattedPlaceLabel.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            formattedPlaceLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: contentSpacing),
+            formattedPlaceLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -contentSpacing),
+            formattedPlaceLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: contentSpacing),
+            formattedPlaceLabel.bottomAnchor.constraint(equalTo: mapView.topAnchor, constant: -contentSpacing),
+            mapView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
